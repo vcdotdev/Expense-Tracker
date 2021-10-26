df96ac8b0beec14a3ee0e5cef345508f4daf02a1
Victor Rajkumar <vrajkumar@informatica.com>
2021-10-26 12:49:52 +0530

    initial commit for frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,6 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+var app = Express();
+app.listen(5000, () => {});
